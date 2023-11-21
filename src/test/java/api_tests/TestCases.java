be42f1251da3edca6bf8a2fc9aab6b2b4e9d5585
Karabo_Maila <karabo.maila@wits.ac.za>
2023-11-21 18:27:47 +0200

    updates

---
+++ src/test/java/api_tests/TestCases.java
@@ -0,0 +1,57 @@
+package api_tests;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestCases {
+
+    @Test
+    public void getAllUsers(){
+        RestAssured.when().get("https://reqres.in/api/users?page=2").then().statusCode(200)
+                .body("data[4].first_name", equalTo("George"));
+
+    }
+
+    @Test
+    public void addUser(){
+        HashMap data = new HashMap<>();
+        data.put("name", "morpheus");
+        data.put("jop", "Leader");
+
+        RestAssured.given().contentType("application/json").accept("application/json").when()
+                .body(data)
+                .post("https://reqres.in/api/users").then().statusCode(201).log().body();
+    }
+
+    @Test
+    public void updateUser(){
+        HashMap data = new HashMap<>();
+        data.put("name", "morpheus");
+        data.put("jop", "Teacher");
+
+        RestAssured.given().contentType("application/json").accept("application/json").when()
+                .body(data)
+                .put("https://reqres.in/api/users/431").then().statusCode(200).log().body();
+    }
+
+    @Test
+    public void patchUser(){
+        HashMap data = new HashMap<>();
+        data.put("name", "morpheus");
+        data.put("jop", "Principal");
+
+        RestAssured.given().contentType("application/json").accept("application/json").when()
+                .body(data)
+                .patch("https://reqres.in/api/users/431").then().statusCode(200).log().body();
+    }
+
+    @Test
+    public void deleteUser(){
+        RestAssured.when()
+                .delete("https://reqres.in/api/users/431").then().statusCode(204).log().body();
+    }
+}
